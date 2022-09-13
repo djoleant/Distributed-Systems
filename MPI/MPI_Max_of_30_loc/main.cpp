6fcaff7b46e131e70e9ee1754da9dccfaaa86198
Djordje Antic <djoleant@iCloud.com>
2022-09-13 04:03:13 +0200

    [MPI] Group operations examples

---
+++ MPI/MPI_Max_of_30_loc/main.cpp
@@ -0,0 +1,40 @@
+/*
+  ┌─────────────────────────────────────────────────────────────────────────┐
+  │ MPI 30 max                                                              │
+  └─────────────────────────────────────────────────────────────────────────┘
+*/
+
+#include <mpi.h>
+#include <iostream>
+
+void main(int argc, char *argv[])
+{
+  struct
+  {
+    double value;
+    int rank;
+  } in[30], out[30]; // in and out buffers
+
+  int size, rank;
+  MPI_Init(&argc, &argv);
+  MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+  for (int i = 0; i < 30; i++)
+  {
+    in[i].value = double(rank + 1);
+    in[i].rank = rank;
+  }
+
+  MPI_Reduce(in, out, 30, MPI_DOUBLE_INT, MPI_MAXLOC, 0, MPI_COMM_WORLD);
+
+  if (rank == 0)
+  {
+    for (int i = 0; i < 30; i++)
+    {
+      std::cout << "Out value " << out[i].value << " at " << out[i].rank << std::endl;
+    }
+  }
+
+  MPI_Finalize();
+}
---
+++ MPI/MPI_min_and_max_1/main.cpp
@@ -0,0 +1,40 @@
+/*
+  ┌─────────────────────────────────────────────────────────────────────────┐
+  │ MPI min and max example                                                 │
+  └─────────────────────────────────────────────────────────────────────────┘
+*/
+
+#include <mpi.h>
+#include <iostream>
+
+void main(int argc, char *argv[])
+{
+  struct
+  {
+    double value;
+    int rank;
+  } in, out; // in and out buffers
+
+  int size, rank;
+  int root;
+  MPI_Init(&argc, &argv);
+  MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+  in.value = rank + 1;
+  in.rank = rank;
+
+  root = 5;
+
+  MPI_Reduce(&in, &out, 1, MPI_DOUBLE_INT, MPI_MAXLOC, root, MPI_COMM_WORLD);
+
+  if (rank == root)
+    std::cout << "Max result is " << out.value << " at " << out.rank << std::endl;
+
+  MPI_Reduce(&in, &out, 1, MPI_DOUBLE_INT, MPI_MINLOC, root, MPI_COMM_WORLD);
+
+  if (rank == root)
+    std::cout << "Min result is " << out.value << " at " << out.rank << std::endl;
+
+  MPI_Finalize();
+}
